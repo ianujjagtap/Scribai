4f72c89d7dac71260e90e58a66f08919b0ec839e
Anuj Jagtap <anujjagtap2004@gmail.com>
2026-02-19 02:32:34 +0530

    chore: fix linting configuration in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import { defineConfig, loadEnv } from "vite";
 
 export default defineConfig(({ mode }) => {
-	const env = loadEnv(mode, ".", "");
+	// const env = loadEnv(mode, ".", "");
 	return {
 		server: {
 			port: 3000,
